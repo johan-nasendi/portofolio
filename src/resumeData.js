839f1c5d1247bf7120f664a74031eb87c36fadea
johannasendi <jhonnasendi@gmail.com>
2021-08-18 14:39:54 +0900

    CodeKeras

---
+++ src/resumeData.js
@@ -0,0 +1,129 @@
+let resumeData = {
+    "name" : "Johan Nasendi",
+    "role": " Web Deveploment",
+    "Telegram": "https://t.me/johannasendi",
+     "Description": "I am a Web Development.",
+
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/johannasendi/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/johannasendi",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"instagram",
+          "url":"https://www.instagram.com/johan.nasendi/",
+          "className":"fa fa-instagram"
+        }, 
+      
+      ],
+
+    "aboutme":"I'm currently in college and learning about IT Informatics, and I'm learning how to web and mobile development, I enjoy being in various parts of web development and enjoy learning about new technologies, writing technical articles.",
+    "address":"Jayapura-Papua-Indonesia",
+    "email":"johannasendi@gmail.com",
+    "no":"+6213-42****30",
+    "website":"https://sacode.web.id/",
+    "education":[
+      {
+        "UniversityName":"Universitas Mulia Of Balikpapan",
+        "specialization":"Informatika S1",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2019",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"SMK NEGERI 1 SENTANI",
+        "specialization":"Teknik Komputer & Jaringan(TKJ)",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2019",
+        "contest1":"2nd Place LKS Papua Province(It software solution for business) in 2018",
+        "contest2":"1nd Place LKS Papua Province(It software solution for business) in 2019"
+      },
+      {
+        "UniversityName":"SMP NEGERI 3 SENTANI",
+        "specialization":"",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2016"
+      },
+      {
+        "UniversityName":"SD YPK BETHESDA MARIBU",
+        "specialization":"",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2014"
+      },
+
+    ],
+    "work":[
+      {
+        "CompanyName":"Maritel Group",
+        "specialization":"Start working",
+        "MonthOfLeaving":"Agu",
+        "YearOfLeaving":"2019",
+        "StopWorking": "Stop working",
+        "MountStop":"Agu",
+        "YearStop":"2020",
+        "Achievements":"Risent"
+      },
+      {
+        "CompanyName":"SaCode",
+        "specialization":"Join Community",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"2020",
+        "Achievements":"Community"
+      },
+    ],
+    
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+        {
+          "skillname":"Framework Laravel"
+        },
+    ],
+    "portfolio":[
+      {
+        "name":"Airport Cargo Port System",
+        "description":"Web",
+        "imgurl":"images/portfolio/coffee.jpg"
+      },
+      {
+        "name":"Web Application PAR BETHESDA MARIBU",
+        "description":"Web",
+        "imgurl":"images/portfolio/console.jpg"
+      },
+      {
+        "name":"Finance Application Web",
+        "description":"Web",  
+        "imgurl":"images/portfolio/retrocam.jpg"
+      },
+      {
+        "name":"Multimedia Application Web",
+        "description":"Web",
+        "imgurl":"images/portfolio/into-the-light.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"We may encounter many defeats but we must not be defeated.",
+        "name":"Maya Angelou"
+      },
+      {
+        "description":"Work is not just to find material. Work is beneficial for many people",
+        "name":"Merry Riana"
+      }
+    ]
+  }
+  
+  export default resumeData
